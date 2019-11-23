c6eb82368c5a0f5dfb24b9652caad4694b26d442
yingruk <yinruk60@gmail.com>
2019-11-23 16:49:42 +0700

    first commit

---
+++ src/th/co/cdgs/bean/CustomerDto.java
@@ -0,0 +1,54 @@
+package th.co.cdgs.bean;
+
+public class CustomerDto {
+	private Long customerId;
+	private String fristName;
+	private String lastName;
+	private String fullName;
+	private String address;
+	private String tel;
+	private String email;
+	public String getFristName() {
+		return fristName;
+	}
+	public void setFristName(String fristName) {
+		this.fristName = fristName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
